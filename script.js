50cf40a4a7a57f9266585424e5b9db170e443640
Nauman <Naumankhattaknaumankhattak@gmail.com>
2024-10-08 08:51:38 -0700

    my portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text", {
+   strings: ["Front-End Developer", "Web Developer", "UI/UX designer"],
+   typeSpeed:100,
+   backspeed:100,
+   backDelay:1000,
+   loop: true 
+});
+
+
+
+
+  
+
+
